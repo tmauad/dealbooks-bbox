0b621add3274115fe5078aa2d7d85033df655260
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2018-02-23 10:38:58 -0300

    Improve relations of `Localizable`

--- app/models/localizable.rb
+++ app/models/localizable.rb
@@ -3,6 +3,7 @@
 class Localizable < ApplicationRecord
   validates :localizable_id, :localizable_type, :location_id, presence: true
 
-  belongs_to :localizable, polymorphic: true
+  belongs_to :localizables, polymorphic: true, inverse_of: :companies
+  belongs_to :localizable, polymorphic: true, inverse_of: :localizables
   belongs_to :location
 end
