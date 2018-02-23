bb3720d678e8437262897ec79f1c847aa83b678d
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2018-02-23 12:14:33 -0300

    Add authentication and devise helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: %i[index show]
+
   protect_from_forgery with: :exception
+
+  # This helper methods is for use a login page, out of the the
+  # controller defined by devise
+  helper_method :resource, :resource_name
+
+  # Overide devise
+  def resource_name
+    :user
+  end
+
+  # Overide devise
+  def resource
+    @resource ||= User.new
+  end
 end
