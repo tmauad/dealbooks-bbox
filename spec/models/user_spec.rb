1d001c76c0ad9ad330315e6fffee2d0d99eca28f
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2018-02-23 11:43:33 -0300

    Add model `User`

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe User do
+  subject(:user) { build(:user) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:role) }
+
+    it do
+      is_expected.to(
+        validate_inclusion_of(:role).in_array(described_class::ROLES)
+      )
+    end
+  end
+end
