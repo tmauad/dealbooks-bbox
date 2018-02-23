245dd35872e1ff438e21450af7351f86e5cc6244
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2018-02-23 14:36:42 -0300

    Add `FaqController`

---
+++ spec/controllers/faq_controller_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+describe FaqController do
+  describe '#index' do
+    subject(:index) { get :index }
+
+    it 'returns 200' do
+      index
+
+      expect(response.status).to eq(200)
+    end
+  end
+end
